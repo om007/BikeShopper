3faa9b9fdd11bc5a334a12a0bfbf18ac0bf6882e
Om <om@ksystem.com.np>
2021-05-24 17:18:09 +0545

    Added lazy loading on list showing 10 shops at a time triggered by user scroll to the bottom of the existing list.

--- BikeShopper/ShopsView.swift
+++ BikeShopper/ShopsView.swift
@@ -12,11 +12,25 @@ struct ShopsView: View {
     
     @EnvironmentObject var currentPosition: Position
     @State var allShops: [Shop] = []
+    @State var isLoading = false
     
     var body: some View {
         NavigationView {
-            List(allShops) { shop in
-                ShopCell(shop: shop)
+            List {
+                ForEach(allShops) { shop in
+                    ShopCell(shop: shop)
+                        .onAppear(perform: {
+                            loadMoreContentIfNeeded(item: shop)
+                        })
+                }
+                
+                if isLoading {
+                    HStack {
+                        Spacer()
+                        ProgressView()
+                        Spacer()
+                    }
+                }
             }
             .navigationBarTitle("Bike Shops", displayMode: .automatic)
         }
@@ -46,15 +60,33 @@ struct ShopsView: View {
                 allShops.append(shopObj)
             }
             
-//            //TO-REMOVE: Creating duplicate data
-//            for _ in 0..<10 {
-//                allShops += allShops
-//            }
-            
             self.allShops = allShops
         })
         
     }
+    
+    func loadMoreContentIfNeeded(item: Shop?) {
+        guard let shop = item else { return loadMoreBikeShops() }
+        
+        //Load more bike shops if the current item falls around the last 2 index items
+        let thresoldIndex = allShops.index(allShops.endIndex, offsetBy: -2)
+        if allShops.firstIndex(where: { $0 === shop}) == thresoldIndex {
+            loadMoreBikeShops()
+        }
+    }
+    
+    func loadMoreBikeShops(){
+        //TO-MODIFY: Creating duplicate data
+        isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+            for i in 0 ..< 10 {
+                allShops.append(Shop(thumbnail: UIImage(systemName: "heart.fill"), locationName: "New Loc \(i)", address: "New address \(i)", coordinate: CommonConsts.initialLocation))
+            }
+            //allShops += allShops[0 ..< min(10, allShops.count) ]
+            isLoading = false
+        })
+    }
+    
 }
 
 struct ShopsView_Previews: PreviewProvider {
@@ -69,7 +101,9 @@ struct ShopCell: View {
     
     var body: some View {
         NavigationLink(destination: ShopDetailView(shop: shop)) {
-            Image(uiImage: shop.thumbnail!)
+            if shop.thumbnail != nil {
+                Image(uiImage: shop.thumbnail!)
+            }
             VStack(alignment: .leading) {
                 Text(shop.locationName)
                     .font(.headline)
@@ -87,7 +121,7 @@ class Shop: Identifiable {
     var address: String = "Address"
     var coordinate: CLLocationCoordinate2D?
     
-    init(thumbnail: UIImage, locationName: String, address: String, coordinate: CLLocationCoordinate2D) {
+    init(thumbnail: UIImage?, locationName: String, address: String, coordinate: CLLocationCoordinate2D) {
         self.thumbnail = thumbnail
         self.locationName = locationName
         self.address = address
